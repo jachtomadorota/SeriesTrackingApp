b599499c416aaf5bd51baf27785ec9abf73b1de3
jachtomadorota <jachtoma.do@gmail.com>
2021-03-28 21:55:31 +0200

    first commit - add the structure of whole project

---
+++ src/main/java/com/dorotajachtoma/seriestrackingapp/SeriesTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.dorotajachtoma.seriestrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesTrackingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesTrackingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dorotajachtoma/seriestrackingapp/SeriesTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorotajachtoma.seriestrackingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesTrackingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
